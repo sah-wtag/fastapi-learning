cdb772a67d773daf67e29528cd7bdd5e9a661e34
Md Sadman Ahmed <sadman.ahmed@welldev.io>
2026-09-06 18:01:23 +0600

    swagger tagging of routes

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from ..database import get_db
 
 
-router = APIRouter(prefix="/posts")
+router = APIRouter(prefix="/posts", tags=["Posts"])
 
 
 @router.get("/", response_model=list[schemas.PostResponse])
--- app/routers/user.py
+++ app/routers/user.py
@@ -4,7 +4,7 @@ from .. import models, schemas, utils
 from ..database import get_db
 
 
-router = APIRouter(prefix="/users")
+router = APIRouter(prefix="/users", tags=['Users'])
 
 
 @router.post(
