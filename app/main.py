bf1146e3cce37d8aa747ffd4bd91704a016d14cd
Md Sadman Ahmed <sadman.ahmed@welldev.io>
2026-09-07 14:30:30 +0600

    code updated

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, Response, status, Depends
 from sqlalchemy.orm import Session
 from . import models, schemas, utils
 from .database import engine, get_db
-from .routers import user, post
+from .routers import user, post, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,6 +11,7 @@ app = FastAPI()
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")
