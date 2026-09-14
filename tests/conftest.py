6034bacaa8de40e8a03715580ff34d6a5e1616d7
Md Sadman Ahmed <sadman.ahmed@welldev.io>
2026-09-14 13:38:19 +0600

    token, authoriazed user, posts created in test db

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,11 @@
 from fastapi.testclient import TestClient
 import pytest
+from app import models
 from app.database import get_db, Base
 from app.main import app
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from app.oauth2 import create_access_token
 
 SQLALCHEMY_DATABASE_URL = "postgresql://welldev:abir1234@localhost/fastapi_test"
 
@@ -45,3 +47,42 @@ def client(session):
     app.dependency_overrides[get_db] = override_get_db
 
     yield TestClient(app)
+
+
+@pytest.fixture()
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+
+
+@pytest.fixture()
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
+
+    return client
+
+
+@pytest.fixture
+    posts_data = [
+        {
+            "title": "First post",
+            "content": " First content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "2nd post",
+            "content": " 2nd content",
+            "owner_id": test_user["id"],
+        },
+        {
+            "title": "3rd post",
+            "content": " 3rd content",
+            "owner_id": test_user["id"],
+        },
+    ]
+
+    posts = [models.Post(**post) for post in posts_data]
+
+    session.add_all(posts)
+    session.commit()
+    posts = session.query(models.Post).all()
+    return posts
